790e7c8aa2189422522a8c98ef7cf18b4b47f7ab
DESKTOP-ITE6VAA\Zelot <khamzat.akhmatkhanov@revolva.ch>
2021-11-09 18:24:57 +0300

    Function Basics

--- public/test.js
+++ public/test.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('test');
+var greet;
+// greet = 'hello'
+greet = function () {
+    console.log('hello, again');
+};
+var add = function (a, b, c) {
+    console.log(a + b);
+    if (c !== undefined) {
+        console.log(c);
+    }
+};
+add(1, 3);
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 7);
+// result = 'something else' // we cannot change the type number
+var minus_2 = function (a, b) {
+    return a - b;
+};
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,27 @@
-console.log('test')
+let greet: Function
+
+// greet = 'hello'
+
+greet = () => {
+    console.log('hello, again')
+}
+
+const add = (a: number, b: number, c?: number | string): void => {
+    console.log(a + b)
+    if (c !== undefined) {
+        console.log(c)
+    }
+}
+
+add(1, 3)
+
+const minus = (a: number, b: number) => {
+    return a - b
+}
+
+let result = minus(10, 7)
+// result = 'something else' // we cannot change the type number
+
+const minus_2 = (a: number, b: number): number => {
+    return a - b
+}
