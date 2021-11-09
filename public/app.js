d47930ba5d1d585044906eb7e024407d1559afa8
DESKTOP-ITE6VAA\Zelot <khamzat.akhmatkhanov@revolva.ch>
2021-11-09 23:51:35 +0300

    Improve Class option declaring

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 // classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string
+    // private details: string // we won't have access to private property by console.log()
+    // public amount: number
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
         return this.client + " owes $" + this.amount + " for " + this.details;
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // classes
 class Invoice {
-    readonly client: string
-    private details: string // we won't have access to private property by console.log()
-    public amount: number
+    // readonly client: string
+    // private details: string // we won't have access to private property by console.log()
+    // public amount: number
 
-    constructor(c: string, d: string, a: number) {
-        this.client = c
-        this.details = d
-        this.amount = a
-    }
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ) { }
 
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`
