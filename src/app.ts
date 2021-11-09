87eeee9611b09433d6d163135a62c4a35e7ff2fd
DESKTOP-ITE6VAA\Zelot <khamzat.akhmatkhanov@revolva.ch>
2021-11-09 23:37:58 +0300

    Declare Invoice Class. Classes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
-// const anchor = document.querySelector('a')!
+// classes
+class Invoice {
+    client: string
+    details: string
+    amount: number
 
-// if (anchor) {
-//     console.log(anchor)
-// }
-// console.log(anchor.href)
+    constructor(c: string, d: string, a: number) {
+        this.client = c
+        this.details = d
+        this.amount = a
+    }
 
-// const form = document.querySelector('form')!
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250)
+const invTwo = new Invoice('luigi', 'work on the mario website', 300)
+
+let invoices: Invoice[] = [] // only Invoices are allowed
+
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invOne.client = 'yoshi'
+invTwo.amount = 400
+
+console.log(invOne, invTwo)
+
+console.log(invoices)
+
+//===================
 const form = document.querySelector('.new-item-form') as HTMLFormElement
-console.log(form.children)
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement
