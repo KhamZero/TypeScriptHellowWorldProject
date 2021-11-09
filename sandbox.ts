243a2b3988c2d83d6f0eb932b7c62281ed9a2556
DESKTOP-ITE6VAA\Zelot <khamzat.akhmatkhanov@revolva.ch>
2021-11-09 14:56:13 +0300

    Objects & Arrays

--- sandbox.ts
+++ sandbox.ts
@@ -1,17 +1,39 @@
-let character = 'mario'
-let age = 30
-let isBlackBelt = false
+//arrays
+let names = ['luigi', 'mario', 'Rick']
 
-character = 'luigi'
+// names = 'hello' // cannot change type of variable
 
-//age = 'yoshi' // We cannot set string value
+names.push('toad')
+// names.push(2) // number is not allowed
 
-isBlackBelt = true
+let numbers = [10, 20, 30, 40]
 
-age = 40
+numbers.push(25)
+// numbers.push('shaun') // string is not allowed
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI
+let mixed = ['ken', 4, 'charlie', 8, 10]
+
+mixed.push('hi')
+mixed.push(20)
+mixed[0] = 1
+
+// objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30,
+
 }
 
-console.log(circ(age))
+ninja.age = 40
+ninja.name = 'charlie'
+// ninja.belt = 10
+// ninja.skills = 'something' // cannot define non-existing properties
+
+ninja = {
+    name: 'rick',
+    belt: 'orange',
+    // age: '200' // we have to have the same structure as before (number type)
+    age: 40,
+    // skills: [] // we have to have the same structure as before (skills property)
+}
