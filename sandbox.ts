30b7cfe866fc3e2969ca2e7364885ee43ecbec7a
DESKTOP-ITE6VAA\Zelot <khamzat.akhmatkhanov@revolva.ch>
2021-11-09 18:06:43 +0300

    Dynamic (any) Types

--- sandbox.ts
+++ sandbox.ts
@@ -1,44 +1,20 @@
-// explicit types
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+let age: any = 25
 
-// age = 'luigi' // age must be a string
-age = 30
+age = '25'
+age = true
+age = { name: 'luigi' }
 
-// isLoggedIn = 25 // isLoggedIn must be a boolean
-isLoggedIn = true;
+let mixed: any[] = []
 
-// arrays
-let ninjas: string[]
-// ninjas = [10, 23] // not string values
-ninjas = ['charlie', 'john']
+mixed.push(5)
+mixed.push('mario')
+mixed.push(false)
+mixed.push({ name: "mario" })
 
-// ninjas.push(1) // cannot push number
-ninjas.push('shaun')
+let ninja: { name: any, age: any }
 
-// union types
-let mixed: (string | number)[];
-mixed.push('some string')
-mixed.push(10)
+ninja = { name: 'yoshi', age: 25 }
+console.log(ninja)
 
-// mixed.push(false) // cannot push boolean
-console.log(mixed)
-
-let uid: string | number
-uid = '12345ad'
-uid = 12345
-
-//objects
-let ninjaOne: object
-ninjaOne = { name: 'yoshi', age: 30 }
-ninjaOne = []
-
-let ninjaTwo {
-    name: string,
-    age: number,
-    beltColour: string
-}
-
-ninjaTwo = { name: name, age: 'someAge', beltColour: 'black' }
-
+ninja = { name: 25, age: 'yoshi' } // we can declare any type of value for `name` and `age`
+console.log(ninja)
