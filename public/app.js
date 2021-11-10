1d34077c1a3cba54b352533753f3cd50eee612a8
DESKTOP-ITE6VAA\Zelot <khamzat.akhmatkhanov@revolva.ch>
2021-11-10 18:27:55 +0300

    Use Interface for project's code

--- public/app.js
+++ public/app.js
@@ -1,39 +1,5 @@
-const me = {
-    name: 'shaun',
-    age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent', amount);
-        return amount;
-    },
-    // skills: [] // skills parameter is not in IsPerson
-};
-const greetPerson = (person) => {
-    console.log('hello', person.name);
-};
-greetPerson(me);
 import { Invoice } from './classes/invoice.js';
 import { Payment } from './classes/Payment.js';
-let docOne;
-let docTwo;
-docOne = new Invoice('yoshi', 'web work', 250);
-docTwo = new Payment('john', 'plumbing work', 200);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the mario website', 300);
-let invoices = []; // only Invoices are allowed
-invoices.push(invOne);
-invoices.push(invTwo);
-docs.push(invOne);
-// invOne.client = 'yoshi' // readonly
-invTwo.amount = 400;
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
 //===================
 const form = document.querySelector('.new-item-form');
 // inputs
@@ -43,5 +9,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,7 @@
-// interfaces
-interface IsPerson {
-    name: string
-    age: number
-    speak(a: string): void
-    spend(a: number): number
-}
-
-const me: IsPerson = {
-    name: 'shaun',
-    age: 30,
-    speak(text: string): void {
-        console.log(text)
-    },
-    spend(amount: number): number {
-        console.log('I spent', amount)
-        return amount
-    },
-    // skills: [] // skills parameter is not in IsPerson
-}
-
-const greetPerson = (person: IsPerson) => {
-    console.log('hello', person.name)
-}
-
-greetPerson(me)
-
-
 import { Invoice } from './classes/invoice.js'
 import { Payment } from './classes/Payment.js'
 import { HasFormatter } from './interfaces/HasFormatter.js'
 
-
-let docOne: HasFormatter
-let docTwo: HasFormatter
-
-docOne = new Invoice('yoshi', 'web work', 250)
-docTwo = new Payment('john', 'plumbing work', 200)
-
-let docs: HasFormatter[] = []
-docs.push(docOne)
-docs.push(docTwo)
-
-const invOne = new Invoice('mario', 'work on the mario website', 250)
-const invTwo = new Invoice('luigi', 'work on the mario website', 300)
-
-let invoices: Invoice[] = [] // only Invoices are allowed
-
-invoices.push(invOne)
-invoices.push(invTwo)
-
-docs.push(invOne)
-
-// invOne.client = 'yoshi' // readonly
-invTwo.amount = 400
-
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format())
-})
 
 //===================
 const form = document.querySelector('.new-item-form') as HTMLFormElement
@@ -71,10 +16,12 @@ const amount = document.querySelector('#amount') as HTMLInputElement
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault()
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    )
+    let doc: HasFormatter
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    } else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    }
+
+    console.log(doc)
 })
